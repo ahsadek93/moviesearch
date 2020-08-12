09f416ac555aa14caa70f72b20363f789730d8a7
Ahmed Sadek <62098165+ahsadek93@users.noreply.github.com>
2020-08-12 02:41:23 +0200

    change repo details for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,10 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+
+  router: {
+    base: '/moviesearch/'
+  }
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
